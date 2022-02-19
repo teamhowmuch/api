fc79dd51079e393723c79fe20bb71ca161ae9f67
pbellinga <pimbellinga@gmail.com>
2022-02-19 17:22:58 +0100

    renames emissions files and adds pseudo code to emissions module

--- src/emissions/emissions.module.ts
+++ src/emissions/emissions.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
-import { EmissionsService } from './determine-emissions.service';
+import { EmissionsService } from './emissions.service';
 
 @Module({
   providers: [EmissionsService]
 })
 export class EmissionsModule {
 
-// 
+// here subscribe to queue ? 
 
+// expenditureCategory = .. from input Expenditure
 
+// instantiate user object from User.... 
+
+// let fuelType = 
+// if fuelType is known from User profile, then fuelType = user.fuelType
+// else: DetermineAdditionalInformation(expenditureCategory) //not for now
+
+// let FuelInLiters: number = EmissionsService.convertFuelExpenditureToLiters(fuelExpenditure)
+
+// let emissionsForFuelInLiters: number =  EmissionsService.calculateEmissionsForFuel(fuelInLiters,fuelType)
+
+// let emissionsForExpenditure: number = emissionsForFuelInLiters;
+
+// return emissionsForExpenditure;
 
 }
