a13c95c802ef95db51cc85e2f66a54eccd3a39a2
pbellinga <pimbellinga@gmail.com>
2022-02-20 09:09:13 +0100

    adds queue producer boilerplate

--- src/emissions/emission-events-producer.service.ts
+++ src/emissions/emission-events-producer.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Queue } from 'bull';
+import { InjectQueue } from '@nestjs/bull';
 
 @Injectable()
-export class EmissionEventsProducerService {}
+export class EmissionEventsProducerService {
+    constructor(@InjectQueue('emissionEvents') private emissionEventsQueue: Queue){
+        
+    }
+}
