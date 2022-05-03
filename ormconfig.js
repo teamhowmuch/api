72749072bc1dfc3c63fc19b7275c40921512fad5
daanaerts <daanaerts@gmail.com>
2022-05-03 16:13:38 +0200

    add ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,18 @@
+module.exports = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DB,
+  logging: false,
+  synchronize: true,
+  entities: ['src/entity/**/*.ts'],
+  migrations: ['src/migration/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+}
