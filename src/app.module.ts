70aaff6c79d8f0c604032bb726ba0531043cbc31
daanaerts <daanaerts@gmail.com>
2022-08-05 14:58:37 +0200

    x

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,13 +14,12 @@ import { RolesGuard } from './auth/roles.guard'
 import { ChatsModule } from './chats/chats.module'
 
 console.log('HELLO')
-console.log(
-  process.env.DB_HOST,
-  process.env.DB_PORT,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  process.env.DB_DB,
-)
+console.log('SSL ON!')
+console.log(process.env.DB_HOST)
+console.log(process.env.DB_PORT)
+console.log(process.env.DB_USER)
+console.log(process.env.DB_PASSWORD)
+console.log(process.env.DB_DB)
 
 @Module({
   imports: [
